bde8b8e5e63d9f7bcb4c468fe7f6608be7b42840
Michael Agee <insaineyesay@gmail.com>
2020-08-08 12:00:11 -0400

    a selection sort that works

--- SelectionSort.playground/Contents.swift
+++ SelectionSort.playground/Contents.swift
@@ -1,25 +1,37 @@
 import UIKit
 
+
+// Utility function to swap values at two indices in the array
+func swap(unsorted: [Int], i: Int, j: Int)
+{
+    var input = unsorted
+    let temp = input[i];
+    input[i] = input[j];
+    input[j] = temp;
+}
+
+// Function to perform selection sort on arr[]
 func selectionSort(_ input: [Int]) -> [Int] {
-    guard input.count > 1 else {
-        return input
-    }
-    
-    var unsorted = input
-    var sorted = Array(repeating: 0, count: unsorted.count) // mark says set the size of this during initialization
-    let count = unsorted.count
-    // for each index in the array
-    var min = unsorted[0]
-    
-    for index in 0..<count {
-        for i in index..<count {
-            if unsorted[i] < min {
-                min = unsorted[i]
-                var temp = unsorted[i]
+    var result = input
+    // run (n - 1) times
+    for index in 1..<result.count {
+        // find the minimum element in the unsorted subarray[i..n-1]
+        // and swap it with arr[i]
+        var min = index;
+        
+        for j in index + 1..<input.count {
+            // if arr[j] element is less, then it is the new minimum
+            if (result[j] < result[min]) {
+                min = j;    // update index of min element
             }
+            
+            // swap the minimum element in subarray[i..n-1] with arr[i]
+            result.swapAt(min, index)
         }
-        return sorted
     }
-    
-    var randomInterviewArray = [2,3,6,8,43,2,23,54,65,45,67,8766,1,433,45,5,73,88,7,655,4,43,21]
-    selectionSort(randomInterviewArray)
+    return result
+}
+
+var randomInterviewArray = [2,3,6,8,43,2,23,54,65,45,67,8766,1,433,45,5,73,88,7,655,4,43,21]
+let result = selectionSort(randomInterviewArray)
+print(result)
